9b4ff4756baf13543faa909589cd16472426b9dc
Michael Rizkalla <rizkalla@yahoo-inc.com>
2014-01-19 09:34:57 -0800

    Initial Commit

---
+++ todo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  todo
+//
+//  Created by Michael Rizkalla on 1/19/14.
+//  Copyright (c) 2014 yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
